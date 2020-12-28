18cf28263031fa8eca4d7b6532e3184fc8fa4365
Peter Klein <kleinpa00@gmail.com>
2020-12-28 04:27:10 +0000

    Tidy up

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ue
 
-archive=maps.tar.gz
+archive=${OUTPUT:-dist/maps.tar.gz}
 
 tmp_dir=$(mktemp -d)
 echo using temporary folder: $tmp_dir >&2
@@ -14,5 +14,6 @@ for dir in */; do
     rsync -a $dir $tmp_dir --exclude 'README*'
 done
 
-tar -C $tmp_dir -czf $archive .
-echo created $archive $(du -kh $archive | cut -f1) >&2
+mkdir -p "$(dirname ${archive})"
+tar -C $tmp_dir -czf "${archive}" .
+echo created "${archive}" $(du -kh "${archive}" | cut -f1) >&2
