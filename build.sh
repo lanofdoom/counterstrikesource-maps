87c62de576181d85fc9efac50d34d92f7caea8c6
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2021-01-02 15:45:17 -0800

    Bzip2 maps for faster downloads

--- build.sh
+++ build.sh
@@ -2,24 +2,40 @@
 
 archive=${OUTPUT:-dist/maps.tar.gz}
 
-tmp_dir=$(mktemp -d)
-echo using temporary folder: $tmp_dir >&2
+archive_tmp_dir=$(mktemp -d)
+downloadurl_tmp_dir=$(mktemp -d)
+echo using temporary folder: $archive_tmp_dir >&2
+echo using temporary folder: $downloadurl_tmp_dir >&2
 clean () {
-    rm -rf $tmp_dir
-    echo deleted temporary folder >&2
+    rm -rf $archive_tmp_dir
+    rm -rf $downloadurl_tmp_dir
+    echo deleted temporary folders >&2
 }
 trap clean EXIT
 
+# Package downloadurl for HTTP mirror
 for dir in */; do
-    rsync -a $dir $tmp_dir --exclude 'README.md' --exclude 'LICENSE'
-    cp $dir/README.md $tmp_dir/${dir%/}_readme.md
-    cp $dir/LICENSE $tmp_dir/${dir%/}_license
+    rsync -a $dir $downloadurl_tmp_dir --exclude 'README.md' --exclude 'LICENSE'
+done
+
+find $downloadurl_tmp_dir -type f -exec bzip2 {} +
+
+for dir in */; do
+    cp $dir/README.md $downloadurl_tmp_dir/${dir%/}_readme.md
+    cp $dir/LICENSE $downloadurl_tmp_dir/${dir%/}_license
+done
+
+# Package tarball for server
+cp -r $downloadurl_tmp_dir/* $archive_tmp_dir/
+
+for dir in */; do
+    rsync -a $dir $archive_tmp_dir --exclude 'README.md' --exclude 'LICENSE'
 done
 
 mkdir -p downloadurl
-cp -r $tmp_dir/* downloadurl/
+cp -r $downloadurl_tmp_dir/* downloadurl/
 echo created downloadurl
 
 mkdir -p "$(dirname ${archive})"
-tar -C $tmp_dir -czf "${archive}" .
+tar -C $archive_tmp_dir -czf "${archive}" .
 echo created "${archive}" $(du -kh "${archive}" | cut -f1) >&2
